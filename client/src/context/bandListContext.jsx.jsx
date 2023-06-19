a6e2f5b73ff06453c3d92a12bab4bc8f7fd78228
domniea <domniea@gmail.com>
2023-06-18 18:10:04 -0600

    Front end Context redirect

--- client/src/context/bandListContext.jsx.jsx
+++ client/src/context/bandListContext.jsx.jsx
@@ -8,7 +8,7 @@ function BandListProvider(props) {
 
     useEffect(() => {
         axios.get('https://bandspace-production.up.railway.app/bands')
-            .then(res => console.log(res))
+            .then(res => setBandList(res.data))
             .catch(err => console.log(err))
     }, [])
 
