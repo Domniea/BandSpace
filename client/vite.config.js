7702c6115d3c512557a8eac13ca47dec7fe044f5
domniea <domniea@gmail.com>
2023-11-07 13:22:44 -0700

    docker container

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,16 +2,28 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:9000',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, '')
+
+//       },
+//     },
+//   },
+// })
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:9000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-
-      },
+    watch: {
+      usePolling: true,
     },
-  },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 8000, // you can replace this port with any port
+  }
 })
